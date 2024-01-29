41e2b606e76aa9e749d1a82bcc530a94f5d8a5be
Viktor <filyk1010@gmail.com>
2024-01-29 18:54:33 +0200

    Add fonts.ts for different fonts exporting

---
+++ src/app/fonts.ts
@@ -0,0 +1,4 @@
+import { Poppins } from "next/font/google"
+
+export const poppins_300 = Poppins({ subsets: ["latin"], weight: "300" })
+export const poppins_600 = Poppins({ subsets: ["latin"], weight: "600" })

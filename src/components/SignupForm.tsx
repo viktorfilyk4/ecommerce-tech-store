09426f5e801181ae0f93cb1174ad7a7dcd69524e
Viktor <filyk1010@gmail.com>
2024-02-09 13:30:49 +0200

    Add /auth route with forms

---
+++ src/components/SignupForm.tsx
@@ -0,0 +1,120 @@
+"use client"
+
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+import {
+  Form,
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  FormField
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button, buttonVariants } from "@/components/ui/button"
+
+const formSchema = z
+  .object({
+    username: z.string().min(3, {
+      message: "Username must be at least 3 characters."
+    }),
+    email: z.string().email("Must be email"),
+    password: z.string().min(8, {
+      message: "Password must be at least 8 characters"
+    }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: "Must be at least 8 characters long" })
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["password", "confirmPassword"]
+  })
+
+type FormSchema = z.infer<typeof formSchema>
+
+export default function SignupForm() {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: ""
+    }
+  })
+
+  const onSubmit = (data: FormSchema) => {
+    console.log("SUCCESS", data)
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='max-w-[500px] mx-auto'
+      >
+        <div className='flex flex-col gap-y-4'>
+          <FormField
+            control={form.control}
+            name='username'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder='Username' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder='Email' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder='Password' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='confirmPassword'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirm password</FormLabel>
+                <FormControl>
+                  <Input placeholder='Confirm password' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className='pt-5'>
+          <Button variant='default' type='submit'>
+            Submit
+          </Button>
+        </div>
+      </form>
+    </Form>
+  )
+}

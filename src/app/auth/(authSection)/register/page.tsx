b703caa4c336f1638a41d136478861254bbe421a
Viktor <filyk1010@gmail.com>
2024-02-09 17:33:51 +0200

    ESlint fix

--- src/app/auth/(authSection)/register/page.tsx
+++ src/app/auth/(authSection)/register/page.tsx
@@ -6,7 +6,7 @@ export default function RegisterPage() {
     <div className='px-6 py-10 flex flex-col gap-y-4 bg-background-secondary'>
       <p className='font-semibold'>Register new account</p>
       <p className='text-xs leading-5'>
-        If you don't have an account, register a new one
+        If you don&apos;t have an account, register a new one
       </p>
       <div>
         <RegisterForm />
